accfe68596a22a67c537e82f9238a6fb8bbf2910
Jonah Stanley <swimming95@aol.com>
2012-06-18 16:08:31 -0400

    Beginnings of moveable lists

--- blocks.js
+++ blocks.js
@@ -2,6 +2,8 @@
 
 function foo(x) { return x * x; }
 var clicked=null
+var listclick=null
+var elements=null
 
 $(document.body).bind('mousemove', 
 		      function(e) {
@@ -9,15 +11,31 @@ $(document.body).bind('mousemove',
 			  		clicked.style.left = e.pageX +"px";
 			  		clicked.style.top = e.pageY +"px";
 				}
+				else if(listclick!=null){
+ 					listclick.style.top= e.pageY + "px"
+				}
 		      });
+
+$(document.body).bind('mouseup',
+					function(){
+						clicked=null;
+						listclick=null;
+						elements=null
+					});
 
 $(".anElement").bind('mousedown', 
 		      function() {
-		      	clicked=this
+		      	clicked=this;
 		      });
 
-$(".anElement").bind('mouseup', 
+
+$(".MoveableItem").bind('mousedown', 
 		      function() {
-		      	clicked=null
+		      	listclick=this;
 		      });
 
+
+$(".MoveableList").bind('mousedown',
+				function() {
+					elements=this.getElementsByTagName("li");
+				});
