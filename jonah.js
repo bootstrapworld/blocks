83c4958245c2f79883f4cb72b73ef5e8a7393f9c
Winnie Wang <winnie_wang@brown.edu>
2012-06-26 11:44:30 -0400

    Changed drawers so that the selectors can't be toggled

--- jonah.js
+++ jonah.js
@@ -1,72 +1,44 @@
- function createFunctionBlock(functionIndex){
- 	var func = functions[functionIndex]
- 	var block = "<table style=\"background: " + colors[func.output] +";\">"
- 	block += "<tr><th class=\"operator\">" + func.name
- 	for(var i = 0; i < func.input.length; i++){
- 		block += "<th style=\"background: " + colors[func.input[i].type] +";\">" + func.input[i].name
- 	}
- 	return block + "</tr></table>"
- }
+//These variables store what the height and width of the code div should be
+var codeHeight = $(document).height() - $("#header").height() - $("#Drawer").height()
+var codeWidth = $(document).width()
 
-function block(str){
- 	if(str === "define-function"){
- 		return createDefineBlock();
- 	} else if (str === "define-constant"){
- 		return createDefineVarBlock();
- 	} else if (str === "define-struct"){
- 		return createDefineStructBlock();
- 	}
- 	 else if (str === "cond"){
- 		return createCondBlock();
- 	}
- 	else{
-	 	for(var i = 0; i < functions.length; i++){
-	 		if (decode(functions[i].name) === str){
-	 			return createFunctionBlock(i);
-	 		}
-	 	}
- 	}
- 	
- }
-
-function createDefineBlock(){
-	var block ="<table style=\"background: " + colors.Define +";\"><tr><th class=\"operator\">define";
-	block+="<th style=\"background: " + colors.Define +"; \"> name <th style=\"background: " + colors.Define +";\">args <th style=\"background: " + colors.Define +";\">expr";
-	return block + "</tr></table>";
+//Sets the width and height of the code div
+function sizeCodeDiv(){
+	$("#code").height(codeHeight);
+	$("#code").width(codeWidth);
 }
 
-function createDefineVarBlock(){
-	var block = "<table style=\"background: " +colors.Define +"; b\"><tr><th class=\"operator\">define";
-	block+="<th style=\"background: " + colors.Define +";\"> name  <th style=\"background: " + colors.Define +";\">expr";
-	return block + "</tr></table>";
+//When the window is resized, the height of the width of the code div changes
+$(window).resize(function(){
+	codeHeight = $(document).height() - $("#header").height() - $("#Drawer").height();
+	codeWidth = $(document).width();
+	sizeCodeDiv();
+});
+
+//DrawerButton takes in an element and either activates (shows) or deactivates (hides) the current element to show the new one
+var activated = null;
+function drawerButton(elt){
+		activated.css("visibility","hidden");
+		activated = $("#options #" + elt.attr('id'))
+		activated.css("visibility", "visible");
 }
 
-function createDefineStructBlock(){
-	var block ="<table style=\"background: " + colors.Define +"; \"><tr><th class=\"operator\">define-struct";
-	block+="<th style=\"background: " + colors.Define +"; \"> name <th style=\"background: " + colors.Define +"; \">properties";
-	return block + "</tr></table>";
-}
+//Upon loading the webpage, the drawers are formed
+$(document).ready(function(){
+	makeDrawers(functions,constants);
+	sizeCodeDiv();
+	activated = $("#options #Numbers")
+	activated.css("visibility", "visible");
+});
 
-function createCondBlock(){
-	var block =  "<table style=\"background: " + colors.Expressions +";\"><tr><th class=\"operator\">cond</tr>";
-	block+="<tr><th><th style=\"background: " + colors.Expressions +"; border: gray;\">boolean <th style=\"background: " + colors.Define +"; border: gray;\">expr</tr>";
-	block+="<tr><th><th style=\"background: " + colors.Expressions +"; border: gray;\">add</th></tr>"
-	return block + "</table>";
-}
-
-
-
+//During the course of the whole session, buttons on the drawer can be clicked in order to add them to the program (the draggable list)
+$("#options span").live('click',function(){
+	var old=document.getElementById("List").innerHTML;
+	document.getElementById("List").innerHTML=old+"<li>"+block($(this).html())+"</li>";
+});
 
 
-table{
-	 border-style: outset; 
-	 border-radius: 10px;
-}
-th{
-	border: gray; 
-	border-style: outset; 
-	border-radius: 10px;
-}
-.operator{
-	border:none;
-}
+//During the course of the whole session, drawers can be opened and closed to reveal all of the buttons (functions) that they contain
+$(".bottomNav").live('click', function(e){
+		drawerButton($(this));
+});
