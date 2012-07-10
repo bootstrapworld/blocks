e690a7286d263273b6c745d67e478f4d1ead7d0b
Jonah Stanley <swimming95@aol.com>
2012-07-10 13:36:05 -0400

    Code review

--- TypeInference.js
+++ TypeInference.js
@@ -1,30 +1,39 @@
 var makeConstraint = function(lhs, rhs){
 	this.lhs = lhs;
 	this.rhs = rhs;
-}
+};
 /*
 *lhs/rhs can be elems or constructed
-*elem is an ID or a type or a makeConstant
+*elem is an ID or a type or a makeVariable
 *Constructed(constructor, List(elem))
 */
 
-makeConstant = function(name){
+// new makeVariable: string -> elem
+makeVariable = function(name){
 	this.name = name;
-}
+};
+
+
+// Represents structured types in the type inference engine.
+// new makeConstruct: string (listof elem) -> elem
 makeConstruct = function(name, elemList){
 	this.name = name;
 	this.elemList = elemList;
-}
+};
+
 error = function(id, message){
 	this.id = id;
 	this.message = message;
-}
+};
+
+
+
+// TODO: eliminate global variables here: make buildConstrains consume these as
+// arguments instead.
 var errors = [];
 var constraints = [];
 
-Array.prototype.addArr = function(input){
-	this[this.lenght] = input;
-}
+
 
 
 function buildConstraints(obj){
@@ -34,11 +43,11 @@ function buildConstraints(obj){
 	var curr;
 	if(obj instanceof makeDefineConst){
         if(obj.expr == null){
-        	errors.addArr(error(obj.funcIDList[0]), "Empty space");
+        	errors.push(error(obj.funcIDList[0]), "Empty space");
         }else{
         	lhs = obj.id;
         	rhs = obj.expr.id;
-        	constraints.addArr(makeConstraint(lhs, rhs))
+        	constraints.push(makeConstraint(lhs, rhs))
         	buildConstraints(obj.expr);
         }
     }else if(obj instanceof makeDefineFunc){
@@ -47,43 +56,43 @@ function buildConstraints(obj){
     	lhs = obj.id;
     	for(var i = 0, i < obj.args.length, i++){
     		if(obj.args[i] == null){
-    			errors.addArr(error(obj.funcIDList[i]), "Empty space");
-    			elemList.addArr(obj.funcIDList[i]);
+    			errors.push(error(obj.funcIDList[i]), "Empty space");
+    			elemList.push(obj.funcIDList[i]);
     		}else{
-    			elemList.addArr(obj.args[i].id);
+    			elemList.push(obj.args[i].id);
     			buildConstraints(obj.args[i]);
     		}
     	}
-    	elemList.addArr(obj.id);
+    	elemList.push(obj.id);
     	rhs = makeConstruct(obj.funcName, elemList);
-    	constraints.addArr(makeConstraint(lhs, rhs));
+    	constraints.push(makeConstraint(lhs, rhs));
     	elemList = [];
     	for(a in functions[(containsName(functions, obj.funcName))].input){
-    		elemList.addArr(a.type);
+    		elemList.push(a.type);
     	}
-    	elemList.addArr(obj.outputType);
-    	constraints.addArr(lhs, makeConstruct(obj.funcName, elemList));
+    	elemList.push(obj.outputType);
+    	constraints.push(lhs, makeConstruct(obj.funcName, elemList));
     }else if(obj instanceof makeNumber
     		|| obj instanceof makeBoolean 
     		|| obj instanceof makeString){
-    	constraints.addArr(obj.id, obj.outputType)
+    	constraints.push(obj.id, obj.outputType)
     }else if(obj instanceof makeConst){
-    	constraints.addArr(obj.id, makeConstant(obj.constName));
+    	constraints.push(obj.id, makeVariable(obj.constName));
     }else if(obj instanceof makeCond){
     	for(var i, i < obj.listOfBooleanAnswer.length, i++){
     		curr = listofBooleanAnswer[i];
     		//NEED RECURSIVE CONSTRAINTS? I.E.: [cond] = [ans1] = [ans2]
     		//or is it fine to have [cond] = [ans1] and [cond]=[ans2]
     		if(curr.answer == null){
-    			errors.addArr(error(curr.id), "Empty Space")
+    			errors.push(error(curr.id), "Empty Space")
     		}else{
-    		constraints.addArr(makeConstraint(obj.id, curr.id))
+    		constraints.push(makeConstraint(obj.id, curr.id))
     		buildConstraints(curr.answer)
     		}
     		if(curr.bool == null){
-    			errors.addArr(error(curr.boolid), "Empty Space")
+    			errors.push(error(curr.boolid), "Empty Space")
     		}else{
-    		constraints.addArr(makeConstraint(curr.boolid, "Booleans"))
+    		constraints.push(makeConstraint(curr.boolid, "Booleans"))
     		buildConstraints(curr.bool)
     		}
     	}
